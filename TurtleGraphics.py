eb83c7ff23e3acf82087308732c60f6a830aac4e
SushanthKSR <149374015+SushanthKSR@users.noreply.github.com>
2024-01-26 00:12:46 +0530

    Add files via upload

---
+++ TurtleGraphics.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+for i in range(10):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
